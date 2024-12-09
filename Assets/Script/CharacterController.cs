74a92123c5b2d5dea58236dede8eaf92652c46e9
voltkrugers <80946089+voltkrugers@users.noreply.github.com>
2024-12-09 11:29:48 +0100

    UseNewInput+AddAsset

--- Assets/Script/CharacterController.cs
+++ Assets/Script/CharacterController.cs
@@ -3,30 +3,11 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     [SerializeField]private float speed = 9f;
+    [SerializeField]private GameInput _gameInput ;
     private bool isWalking;
     void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
-
+        Vector2 inputVector = _gameInput.GetMouvementVector();
         Vector3 moveDir = new Vector3(inputVector.x, 0f,inputVector.y );
         transform.position += moveDir * Time.deltaTime * speed;
 
---
+++ Assets/Script/GameInput.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions _playerInputActions;
+    private void Awake()
+    {
+        _playerInputActions = new PlayerInputActions();
+        _playerInputActions.Player.Enable();
+    }
+
+    public Vector2 GetMouvementVector()
+    {
+        Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
+
+
+        inputVector = inputVector.normalized;
+        return inputVector;
+    }
+}
